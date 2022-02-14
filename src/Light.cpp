8dfef07c7caa7ae3ff1f395a80bb6a86abcf6b16
Vynokris <vynokris@gmail.com>
2022-02-14 09:16:47 +0100

    Fixed compilation issues and smoothed the fps meter.

--- src/Light.cpp
+++ src/Light.cpp
@@ -1,12 +1,12 @@
 #include "my_math.hpp"
 
-#include "light.hpp"
+#include "Light.hpp"
 
 Light::Light() { params = { Vector3(), 0.f, 0.f, 0.f }; }
 
-Light:Light(const LightParams& p) : params(p) {}
+Light::Light(const LightParams& p) : params(p) {}
 
-void Light::setParams(LightParams& p)
+void Light::setParams(const LightParams& p)
 {
     params = p;
 }
