20c665801bb78cd90a7f487e7069e3d25478886a
vegasword <a.perche@student.isartdigital.com>
2022-02-23 00:25:02 +0100

    UI remake in progress.

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,9 +1,12 @@
+#include <string>
+
 #include <imgui.h>
 #include <my_math.hpp>
 
 #include <Light.hpp>
 #include <Scene.hpp>
 
+using namespace std;
 using namespace geometry3D;
 
 Scene::Scene()
@@ -41,45 +44,29 @@ void Scene::update(const float& _deltaTime, Renderer& _renderer, const Camera& _
     _renderer.setModel(getTransformMatrix({ 0, 0, 0 }, { 0, 0, 0 }, { scale, scale, scale }));
     switch (_camera.getViewMode())
     {
-    case ViewMode::FIRST_PERSON:
-        _renderer.setView(_camera.getViewMat());
-        break;
-    case ViewMode::THIRD_PERSON:
-        _renderer.setView(_camera.getLookAtMat({ 0, 0, 2 }));
-        break;
-    default: break;
+        case ViewMode::FIRST_PERSON: _renderer.setView(_camera.getViewMat());              break;
+        case ViewMode::THIRD_PERSON: _renderer.setView(_camera.getLookAtMat({ 0, 0, 2 })); break;
+        default: break;
     }
     _renderer.setProjection(_camera.getPerspective());
 
-    // Draw the first triangle.
-    // _renderer.modelPushMat();
-    // _renderer.modelTranslate(0, 0, 3);
-    // _renderer.drawTriangles(&triangles[0], 1);
-    // _renderer.modelPopMat();
-
-    // Draw the second triangle.
-    // _renderer.modelPushMat();
-    // _renderer.modelTranslate(0, 0, 3);
-    // _renderer.drawTriangles(&triangles[1], 1);
-    // _renderer.modelPopMat();
-
     // Draw cube.
     _renderer.modelPushMat();
-    _renderer.modelTranslate(0, 0, 2);
-    _renderer.modelRotateX(fmod(time/2, 2*PI));
-    _renderer.modelRotateY(fmod(time/2, 2*PI));
-    _renderer.modelRotateZ(fmod(time/2, 2*PI));
+    _renderer.modelTranslate(-2, 0, 2);
+    //_renderer.modelRotateX(fmod(time/2, 2*PI));
+    //_renderer.modelRotateY(fmod(time/2, 2*PI));
+    //_renderer.modelRotateZ(fmod(time/2, 2*PI));
     // _renderer.drawCube({ 1, 1, 1, 1 });
-    _renderer.drawDividedCube({ 1, 1, 1, 0.5 }, 1, 10);
+    _renderer.drawDividedCube({ 1, 1, 1, 1 }, 1, 10);
     _renderer.modelPopMat();
 
     // Draw sphere.
     _renderer.modelPushMat();
-    _renderer.modelTranslate(0, 0, 2);
+    _renderer.modelTranslate(2, 0, 2);
     _renderer.modelRotateX(-fmod(time/2, 2*PI));
     _renderer.modelRotateY(-fmod(time/2, 2*PI));
     _renderer.modelRotateZ(-fmod(time/2, 2*PI));
-    _renderer.drawSphere({ 1, 1, 1, 1 }, 0.7, 6, 6);
+    _renderer.drawSphere({ 1, 1, 1, 1 }, 0.7, 32, 32);
     _renderer.modelPopMat();
     
     
@@ -89,4 +76,23 @@ void Scene::update(const float& _deltaTime, Renderer& _renderer, const Camera& _
 
 std::vector<Light> Scene::getLights() const { return lights; }
 
-void Scene::showImGuiControls() { }
+void Scene::showImGuiControls()
+{
+    // Display all lights
+    if (ImGui::CollapsingHeader("Lights", ImGuiTreeNodeFlags_DefaultOpen))
+    {
+        static int index = 0;
+        for (Light light : lights)
+        {
+            index++;
+
+            string lightName = "Light ";
+            lightName += to_string(index);
+            if (ImGui::CollapsingHeader(lightName.c_str(), ImGuiTreeNodeFlags_DefaultOpen))
+            {
+                // TODO (VALUES AND SLIDERS).
+            }
+        }
+        index = 0;
+    }
+}
